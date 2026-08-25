73463dac139b17550633514b75303e763d71eb26
pedroobando <pedroobando@hotmail.com>
2026-08-24 22:57:52 -0400

    Paso - 9 feat(rooms): create index file to export room-related actions and types

---
+++ app/actions/rooms/index.ts
@@ -0,0 +1,11 @@
+export { listRooms } from './list-rooms';
+export { createRoom } from './create-room';
+export { updateRoom } from './update-room';
+export { deleteRoom } from './delete-room';
+export type {
+  RoomRow,
+  RoomInsert,
+  RoomUpdate,
+  CreateRoomState,
+  UpdateRoomState,
+} from './types';
