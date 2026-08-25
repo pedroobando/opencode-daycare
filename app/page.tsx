1f5708a9db03f9be07a6fbf76a1a1b4175a6f50a
pedroobando <pedroobando@hotmail.com>
2026-08-24 20:52:04 -0400

    Paso 13 - feat: Implement user authentication check and redirect in HomePage

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,16 @@
+import { redirect } from 'next/navigation';
 import { FeedBody } from '@/app/components/feed/FeedBody';
+import { createSupabaseServerClient } from '@/lib/supabase/server';
 
-export default function HomePage() {
+export default async function HomePage() {
+  const supabase = await createSupabaseServerClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    redirect('/auth');
+  }
+
   return <FeedBody />;
 }
